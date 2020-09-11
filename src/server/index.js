91fb4f1e047ab73ea0e27cd5ed075ec3f0405d64
Deo-Mac <druplall@gmail.com>
2020-09-11 19:55:54 -0400

    Added react routing

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../../dist')));
 
+app.get('*', (req, res) => {
+  // Handles any requests that don't match the ones above
+  res.sendFile('index.html', { root: path.join(__dirname, '../../dist/') });
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
