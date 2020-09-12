fb7f36817c88c8d20082e8c838f77db8da9b3b74
Liam Murray <murrayl9797@gmail.com>
2020-09-12 16:22:58 -0400

    Refactor dataset to be array of objects with 'x' and 'y' properties

--- src/client/river/index.js
+++ src/client/river/index.js
@@ -3,36 +3,39 @@ import React, {useState, useEffect} from 'react';
 
 
 const generateDataset = () => (
-  Array(200).fill(0).map(() => ([
-    Math.random() * 100,
-    Math.random() * 100,
-  ]))
+  Array(200).fill(0).map(() => ({
+    x: Math.random() * 100,
+    y: Math.random() * 100,
+  }
+  ))
+
 )
 
 const Chart = () => {
     const [dataset, setDataset] = useState(
       generateDataset()
-    )
+    );
 
+    console.log(dataset);
     return (
 
       <svg className='river-container'width='50%' height='50%' viewBox="0 0 100 100">
         <rect width="100%" height="100%" fill="black" />
-        {dataset.map(([x, y], i) => (
+        {dataset.map(({x, y}, i) => (
           <>
-          <defs>
-          <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-            <stop offset="0%" stopColor='rgb(255,255,255)' stopOpacity='1' />
-            <stop offset="100%" stopColor='rgb(0,0,255)' stopOpacity='0' />
-          </radialGradient>
-        </defs>
+            <defs>
+              <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+                <stop offset="0%" stopColor='rgb(255,255,255)' stopOpacity='1' />
+                <stop offset="100%" stopColor='rgb(0,0,255)' stopOpacity='0' />
+              </radialGradient>
+            </defs>
 
-          <circle
-            cx={x}
-            cy={y}
-            r=".7"
-            fill="url(#grad1)"
-          />
+            <circle
+              cx={x}
+              cy={y}
+              r=".7"
+              fill="url(#grad1)"
+            />
           </>
         ))}
       </svg>
