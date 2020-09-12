527ef022fc2286fa46a640dbcb9fe92cacd80078
Anna Makagonova <makagonova.ann@gmail.com>
2020-09-12 10:50:47 -0400

    add header, move images folder

--- src/client/app.js
+++ src/client/app.js
@@ -8,6 +8,7 @@ const App = (props) => {
   return (
     <Router>
       <div>
+        <header></header>
         <div className="menu">
           <Link to="/">Home</Link>
           <Link to="/rivers">Meditation River</Link>
@@ -35,21 +36,3 @@ const App = (props) => {
 };
 
 export default App;
-
-<ul>
-  <li>
-    <Link to="/">Home</Link>
-  </li>
-  <li>
-    <Link to="/leaderboard">LeaderBoard</Link>
-  </li>
-  <li>
-    <Link to="/bulletinboard">Bulletinboard</Link>
-  </li>
-  <li>
-    <Link to="/login">Login</Link>
-  </li>
-  <li>
-    <Link to="/rivers">Rivers</Link>
-  </li>
-</ul>;
