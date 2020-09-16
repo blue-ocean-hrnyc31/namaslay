15780ab89f07e48bfe2d47a27f40e256edc9fe62
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2020-09-16 16:05:21 -0400

    redirect for meditation river

--- src/client/login/protectedRoute.js
+++ src/client/login/protectedRoute.js
@@ -1,17 +1,20 @@
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useAuth } from './auth';
 
-const ProtectedRoute = ({ component: Component, isLoggedIn, ...rest }) => {
+const ProtectedRoute = ({ component: Component, handlePath, isLoggedIn, ...rest }) => {
   const { authTokens } = useAuth();
   console.log('authTokens:', authTokens);
 
+  const location = useLocation();
+  console.log('this is in protected route', location);
+  handlePath(location.pathname);
   return (
     <Route
       {...rest}
       render={(props) =>
-        authTokens ? <Component {...props} /> : <Redirect to='/login' />
+        authTokens ? <Component {...rest} {...props} /> : <Redirect to='/login' />
       }
     />
   );
