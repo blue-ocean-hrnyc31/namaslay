03f51f9df4ec9c2a1ffbf4252a2578ad4e67549f
xdynasty <xdynasty5201314@gmail.com>
2020-09-15 22:36:09 -0400

    add webpack.prod.config and reset api urls

--- src/client/Menu.js
+++ src/client/Menu.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Menu = (props) => (
-  <div className='menu'>
-    <Link to='/'>Home</Link>
-    <Link to='/meditation-river'>Meditation River</Link>
-    <Link to='/asana-river'>Asana River</Link>
-    <Link to='/bulletinboard'>Upcoming Events</Link>
-    <Link to='/leaderboard'>Leader Board</Link>
-    <Link to='/about'>About</Link>
-    {(!props.authTokens || props.authTokens === 'undefined') && (
-      <Link to='/login'>Log In</Link>
-    )}
-    {props.authTokens && props.authTokens !== 'undefined' && (
-      <Link to='/logout'>Log Out</Link>
-    )}
-  </div>
-);
+const Menu = (props) => {
+  let path = useLocation().pathname;
+  if (path !== '/') {
+    return (
+      <div className='menu'>
+        <Link to='/'>Home</Link>
+        <Link to='/meditation-river'>Meditation River</Link>
+        <Link to='/asana-river'>Asana River</Link>
+        <Link to='/bulletinboard'>Upcoming Events</Link>
+        <Link to='/leaderboard'>Leader Board</Link>
+        <Link to='/about'>About</Link>
+        {!props.authTokens && <Link to='/login'>Log In</Link>}
+        {props.authTokens && <Link to='/logout'>Log Out</Link>}
+      </div>
+    );
+  }
+  return null;
+};
 
 export default Menu;
--- src/client/apiHelpers/logOut.js
+++ src/client/apiHelpers/logOut.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function logOut() {
   return axios
-    .post('http://localhost:3000/logout')
+    .post('/logout')
     .then(({ data }) => {
       // console.log('logout data:', data);
       return data.login;
