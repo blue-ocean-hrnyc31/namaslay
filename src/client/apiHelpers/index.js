3a0021c07569614ed0137dd320afd41ad9570bb5
Matthew Faris <mtfaris90@gmail.com>
2020-09-16 16:12:31 -0400

    add frontend request for upcoming events data

--- src/client/apiHelpers/index.js
+++ src/client/apiHelpers/index.js
@@ -2,5 +2,6 @@ import logIn from './logIn';
 import signUp from './signUp';
 import logOut from './logOut';
 import countPracticing from './countPracticing';
+import getUpcoming from './getUpcoming';
 
-export { logIn, signUp, logOut, countPracticing };
+export { logIn, signUp, logOut, countPracticing, getUpcoming };
