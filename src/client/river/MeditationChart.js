df3f8dd59d7de1651590996100904aed9ec28561
Deo-Mac <druplall@gmail.com>
2020-09-16 15:23:36 -0400

    Added hover over on river components

--- src/client/river/MeditationChart.js
+++ src/client/river/MeditationChart.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
-import * as d3 from "d3";
+import React, { useState, useEffect } from 'react';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
+import * as d3 from 'd3';
 
 const generateDataset = (userList) =>
   Array(userList.length)
@@ -16,24 +18,25 @@ const generateDataset = (userList) =>
     });
 
 const MeditationChart = ({ allUsersInMeditation }) => {
-  const [dataset, setDataset] = useState(generateDataset(allUsersInMeditation ));
+  const [dataset, setDataset] = useState(generateDataset(allUsersInMeditation));
   useEffect(() => {
-    setDataset(generateDataset(allUsersInMeditation))
+    setDataset(generateDataset(allUsersInMeditation));
   }, [allUsersInMeditation]);
 
   const [hoveredObj, updateHovered] = useState({
     isHovered: false,
-    username: "No one",
-    location: "No where",
-    current_activity: "Not existing",
+    username: 'No one',
+    location: 'No where',
+    current_activity: 'Not existing',
+    id: '',
   });
 
   //console.log(dataset);
 
   return (
-    <div className="river-view-container">
-      <div className="river-container">
-        <svg width="90%" height="90%" viewBox="0 0 100 100">
+    <div className='river-view-container'>
+      <div className='river-container'>
+        <svg width='90%' height='90%' viewBox='0 0 100 100'>
           {hoveredObj.isHovered ? (
             <p>
               {hoveredObj.username} , {hoveredObj.location}
@@ -41,38 +44,40 @@ const MeditationChart = ({ allUsersInMeditation }) => {
           ) : (
             <p></p>
           )}
-          <rect width="100%" height="100%" fill="black" />
+          <rect width='100%' height='100%' fill='black' />
           {dataset.map(({ x, y, username, location, current_activity }, i) => (
             <>
               <defs>
                 <radialGradient
-                  id="grad1"
-                  cx="50%"
-                  cy="50%"
-                  r="50%"
-                  fx="50%"
-                  fy="50%"
+                  id='grad1'
+                  cx='50%'
+                  cy='50%'
+                  r='50%'
+                  fx='50%'
+                  fy='50%'
                 >
                   <stop
-                    offset="0%"
-                    stopColor="rgb(255,255,255)"
-                    stopOpacity="1"
+                    offset='0%'
+                    stopColor='rgb(255,255,255)'
+                    stopOpacity='1'
                   />
                   <stop
-                    offset="100%"
-                    stopColor="rgb(0,0,255)"
-                    stopOpacity="0"
+                    offset='100%'
+                    stopColor='rgb(0,0,255)'
+                    stopOpacity='0'
                   />
                 </radialGradient>
               </defs>
 
               <circle
-                onMouseEnter={() => {
+                id={`circle-${i}`}
+                onMouseEnter={(e) => {
                   updateHovered({
                     isHovered: true,
                     username,
                     location,
                     current_activity,
+                    id: e.target,
                   });
                 }}
                 onMouseLeave={() => {
@@ -85,26 +90,27 @@ const MeditationChart = ({ allUsersInMeditation }) => {
                 }}
                 cx={x}
                 cy={y}
-                r="1.3"
-                fill="url(#grad1)"
+                r='1.3'
+                fill='url(#grad1)'
               />
             </>
           ))}
         </svg>
       </div>
-      <div className="river-users-info">
-        <div className="users-info">
-          {hoveredObj.isHovered ? (
-            <span>
-              {hoveredObj.username}: {hoveredObj.current_activity} in{" "}
+      <div>
+        <Overlay
+          show={hoveredObj.isHovered}
+          target={hoveredObj.id}
+          placement='top'
+        >
+          <Popover id='popover-contained'>
+            <Popover.Title as='h3'>
+              {hoveredObj.username} {''} {hoveredObj.current_activity} {''}
+              {' in '}
               {hoveredObj.location}
-            </span>
-          ) : (
-            <span className="placeholder">
-              Hover over the dots in the river to see other users' status
-            </span>
-          )}
-        </div>
+            </Popover.Title>
+          </Popover>
+        </Overlay>
       </div>
     </div>
   );
@@ -114,23 +120,23 @@ export default MeditationChart;
 
 const dummyUsers = [
   {
-    username: "Liam",
-    location: "NYC",
-    current_activity: "Chilllllllllin",
+    username: 'Liam',
+    location: 'NYC',
+    current_activity: 'Chilllllllllin',
   },
   {
-    username: "Bobbito",
-    location: "Cali",
-    current_activity: "Shredding gnar",
+    username: 'Bobbito',
+    location: 'Cali',
+    current_activity: 'Shredding gnar',
   },
   {
-    username: "Nuri",
-    location: "NYC",
-    current_activity: "Just vibingggg",
+    username: 'Nuri',
+    location: 'NYC',
+    current_activity: 'Just vibingggg',
   },
   {
-    username: "Trent",
-    location: "NYC",
-    current_activity: "Beep booping",
+    username: 'Trent',
+    location: 'NYC',
+    current_activity: 'Beep booping',
   },
 ];
