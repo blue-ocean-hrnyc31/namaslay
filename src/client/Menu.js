be96ff289649a484d61fc2f9a30191081f94b11e
Matthew Faris <mtfaris90@gmail.com>
2020-09-16 11:18:20 -0400

    create and use axios request for practicers info and clean up styling errors on about page

--- src/client/Menu.js
+++ src/client/Menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
@@ -28,26 +27,5 @@ const Menu = (props) => {
     return null;
   }
 };
-=======
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Menu = (props) => (
-  <div className='menu'>
-    <Link to='/'>Home</Link>
-    <Link to='/meditation-river'>Meditation River</Link>
-    <Link to='/asana-river'>Asana River</Link>
-    <Link to='/bulletinboard'>Upcoming Events</Link>
-    <Link to='/leaderboard'>Leader Board</Link>
-    <Link to='/about'>About</Link>
-    {(!props.authTokens || props.authTokens === 'undefined') && (
-      <Link to='/login'>Log In</Link>
-    )}
-    {props.authTokens && props.authTokens !== 'undefined' && (
-      <Link to='/logout'>Log Out</Link>
-    )}
-  </div>
-);
->>>>>>> master
 
 export default Menu;
