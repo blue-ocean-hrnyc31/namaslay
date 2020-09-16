fe4824a655c03041e8be0a8d14b633b39edb4fc1
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2020-09-16 18:49:19 -0400

    Move menu to top in Home, Visible on hover

--- src/client/Menu.js
+++ src/client/Menu.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './stylesheets/menu.scss';
 
 const Menu = (props) => {
   let path = useLocation().pathname;
-  if (path !== '/') {
+
+  if (path === '/') {
+
     return (
-      <div className='menu'>
-        <Link to='/'>Home</Link>
-        <Link to='/meditation-river'>Meditation River</Link>
-        <Link to='/asana-river'>Asana River</Link>
-        <Link to='/bulletinboard'>Upcoming Events</Link>
-        <Link to='/leaderboard'>Leader Board</Link>
-        <Link to='/about'>About</Link>
-        {!props.authTokens && <Link to='/login'>Log In</Link>}
-        {props.authTokens && <Link to='/logout'>Log Out</Link>}
+      <div id='menu-wrapper'>
+        <div className='menu' >
+          <Link to='/'>Home</Link>
+          <Link to='/meditation-river'>Meditation River</Link>
+          <Link to='/asana-river'>Asana River</Link>
+          <Link to='/bulletinboard'>Upcoming Events</Link>
+          <Link to='/leaderboard'>Leader Board</Link>
+          <Link to='/about'>About</Link>
+          {!props.authTokens && <Link to='/login'>Log In</Link>}
+          {props.authTokens && <Link to='/logout'>Log Out</Link>}
+        </div>
       </div>
     );
   }
-  return null;
+
+
+  return (
+    <div className='menu' >
+      <Link to='/'>Home</Link>
+      <Link to='/meditation-river'>Meditation River</Link>
+      <Link to='/asana-river'>Asana River</Link>
+      <Link to='/bulletinboard'>Upcoming Events</Link>
+      <Link to='/leaderboard'>Leader Board</Link>
+      <Link to='/about'>About</Link>
+      {!props.authTokens && <Link to='/login'>Log In</Link>}
+      {props.authTokens && <Link to='/logout'>Log Out</Link>}
+    </div>
+  );
+
 };
 
 export default Menu;
