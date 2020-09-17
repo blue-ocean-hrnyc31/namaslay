d25ae3f77a94c0f240408288cb51b1ed8425ec0a
xdynasty <xdynasty5201314@gmail.com>
2020-09-17 11:07:02 -0400

    add puppeteer testing

---
+++ test/test.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+describe('Home Page', () => {
+  const host = 'http://127.0.0.1:9000';
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto(host);
+
+    page.on('console', (msg) => {
+      console.log('msg:', msg);
+      if (msg.type() === 'error') {
+        console.log('console msg:', msg.text());
+      }
+    });
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it('should render two links in the home page', async () => {
+    const links = await page.$$('a');
+    expect(links.length).toBe(9);
+  });
+});
