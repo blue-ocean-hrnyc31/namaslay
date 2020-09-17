6269dc4db2d1e1e4c52df890a4fe5c6c38cad236
xdynasty <xdynasty5201314@gmail.com>
2020-09-17 11:23:12 -0400

    add supertest

--- test/test.js
+++ test/test.js
@@ -33,7 +33,21 @@ describe('Home Page', () => {
 
   it('should render two links in the home page', async () => {
     const links = await page.$$('a');
-    console.log(links.length);
+    //console.log(links.length);
     expect(links.length).toBe(9);
+  });
+});
+
+describe('Database', () => {
+  const host = 'http://34.229.137.235:4444';
+
+  it('should return an object when getting /countPracticing', () => {
+    return request(host)
+      .get('/countPracticing')
+      .expect(200)
+      .then(({ body }) => {
+        //console.log('body:', body);
+        expect(body[0]).toHaveProperty('count');
+      });
   });
 });
